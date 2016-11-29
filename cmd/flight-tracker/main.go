7086d6110e0fff7ea844957e870735c2936fdb5c
Donald Guy <donald@tulip.co>
2016-11-28 20:00:36 -0500

    CLI to describe the build from a past git commit

--- cmd/flight-tracker/main.go
+++ cmd/flight-tracker/main.go
@@ -3,27 +3,28 @@ package main
 import (
 	"fmt"
 	"os"
-	"time"
 
-	"github.com/concourse/atc"
 	"github.com/concourse/fly/rc"
+	"github.com/donaldguy/flightplan"
+	"github.com/fatih/color"
 	flags "github.com/jessevdk/go-flags"
+	git "gopkg.in/libgit2/git2go.v24"
 )
-
-type duration time.Duration
-
-func (freq *duration) UnmarshalFlag(value string) (err error) {
-	f, err := time.ParseDuration(value)
-	*freq = duration(f)
-	return
-}
 
 type Options struct {
 	Version func() `short:"v" long:"version" description:"Print the version and exit"`
 
-	Target rc.TargetName `short:"t" long:"target" description:"Fly target to monitor" env:"FLIGHT_TRACKER_SERVER"`
+	Target   rc.TargetName `short:"t" long:"target" description:"Fly target to monitor" env:"FLIGHT_TRACKER_SERVER" required:"true"`
+	RepoPath string        `short:"r" long:"git-repo-path" description:"Path to a local checkout of the git repo you are tracking" required:"true"`
+	Branch   string        `short:"b" long:"branch" description:"The name of the branch you are tracking" default:"master"`
+	Pipeline string        `short:"p" long:"pipeline" description:"Name of pipeline you are tracking. Defaults to name of branch"`
+}
 
-	PollFreq duration `short:"f" long:"polling-frequency" default:"5s"`
+func dieIf(err error) {
+	if err != nil {
+		fmt.Fprintln(os.Stderr, err)
+		os.Exit(1)
+	}
 }
 
 func main() {
@@ -34,23 +35,54 @@ func main() {
 	}
 
 	parser := flags.NewParser(&opts, flags.HelpFlag|flags.PassDoubleDash)
-	parser.NamespaceDelimiter = "-"
 
-	//twentythousandtonnesofcrudeoil.TheEnvironmentIsPerfectlySafe(parser, "FLIGHT_TRACKER_")
-
-	_, err := parser.Parse()
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
-		os.Exit(1)
-	}
+	args, err := parser.Parse()
+	dieIf(err)
 
 	target, err := rc.LoadTarget(opts.Target)
-	if err != nil {
-		fmt.Fprintln(os.Stderr, err)
+	dieIf(err)
+
+	if len(args) < 1 {
+		fmt.Fprintln(os.Stderr, "You must provide a commit to describe")
 		os.Exit(1)
 	}
+	if opts.Pipeline == "" {
+		opts.Pipeline = opts.Branch
+	}
 
-	apiClient := target.Client()
-	newBuilds := make(chan atc.Build)
-	go buildPoller(&apiClient, &newBuilds)
+	teamClient := target.Team()
+	pipelineClient := PipelineClient{
+		Client:       target.Client(),
+		Team:         target.Team(),
+		PipelineName: opts.Pipeline,
+	}
+
+	repo, err := git.OpenRepository(opts.RepoPath)
+	dieIf(err)
+	obj, err := repo.RevparseSingle(args[0])
+	dieIf(err)
+	gCommit, err := obj.AsCommit()
+	dieIf(err)
+
+	commit := flightplan.GitCommit{Repo: repo, Commit: gCommit}
+
+	bold := color.New(color.Bold).PrintfFunc()
+	bold("The story of %s:\n\n", commit.Id())
+	shaShort := commit.Id().String()[0:7]
+	fmt.Printf("%s was written   on %s by %s\n", shaShort, commit.Author().When, commit.Author().Name)
+	fmt.Printf("        and committed on %s by %s\n\n", commit.Committer().When, commit.Committer().Name)
+
+	pipeline, err := flightplan.NewPipeline(teamClient, opts.Pipeline)
+	dieIf(err)
+	resources, err := commit.ResourcesTriggeredIn(pipeline)
+	fmt.Printf("Thus it would come to trigger the following resources: %v\n", color.CyanString("%v", resources))
+
+	for _, rName := range resources {
+		resourceName := string(rName)
+		fmt.Printf("\nOf %s:\n", color.CyanString("%s", resourceName))
+		rv, err := pipelineClient.gitSha2ResourceVersion(resourceName, commit.Id().String())
+		dieIf(err)
+
+		fmt.Printf("%s", pipelineClient.describeResourceJourney(rv))
+	}
 }
---
+++ cmd/flight-tracker/pipeline_client.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"bytes"
+	"fmt"
+	"time"
+
+	"github.com/concourse/atc"
+	"github.com/concourse/go-concourse/concourse"
+	"github.com/fatih/color"
+)
+
+type PipelineClient struct {
+	concourse.Team
+	concourse.Client
+	PipelineName string
+}
+
+func (pc *PipelineClient) gitSha2ResourceVersion(resourceName, sha string) (*atc.VersionedResource, error) {
+	rvs, _, any, err := pc.Team.ResourceVersions(pc.PipelineName, resourceName, concourse.Page{Limit: 10})
+	if err != nil {
+		return nil, err
+	}
+
+	if any {
+		if rvs[0].Type != "git" {
+			return nil, fmt.Errorf("Resource named %s doesn't appear to be a git repo!", resourceName)
+		}
+	}
+
+	for _, rv := range rvs {
+		for _, rvm := range rv.Metadata {
+			if rvm.Name == "commit" && rvm.Value == sha {
+				return &rv, nil
+			}
+		}
+	}
+
+	return nil, fmt.Errorf("No version of %s in %s found with sha %s", resourceName, pc.PipelineName, sha)
+}
+
+func (pc *PipelineClient) describeResourceJourney(rv *atc.VersionedResource) []byte {
+	var b bytes.Buffer
+
+	builds, _, err := pc.Team.BuildsWithVersionAsInput(pc.PipelineName, rv.Resource, rv.ID)
+	dieIf(err)
+	for _, build := range builds {
+		fmt.Fprintf(&b, "it triggered %s #%s, which %s after %s",
+			color.YellowString("%s", build.JobName),
+			build.Name,
+			build.Status,
+			(time.Duration(build.EndTime-build.StartTime) * time.Second).String(),
+		)
+
+		rio, _, err := pc.Client.BuildResources(build.ID)
+		dieIf(err)
+		for _, input := range rio.Inputs {
+			if input.Resource != rv.Resource {
+				fmt.Fprintf(&b,
+					"  (it also consumed a %s)\n",
+					color.CyanString("%s", input.Resource),
+				)
+			}
+		}
+		for _, output := range rio.Outputs {
+			fmt.Fprintf(&b,
+				"  It output %s - %s:\n",
+				color.CyanString("%s", output.Resource),
+				output.Version,
+			)
+			b2 := pc.describeResourceJourney(&output)
+			for _, line := range bytes.Split(b2, []byte("\n")) {
+				fmt.Fprintf(&b, "  %s", line)
+			}
+		}
+
+	}
+
+	return b.Bytes()
+}
